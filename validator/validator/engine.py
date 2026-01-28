55fdeaa5efb4a60f363d45b735e677169f13a36e
Ptaprogramator87 <pascu.alexandru87@gmail.com>
2026-01-28 11:41:18 +0200

    Add SAF-T validator engine (dummy)

---
+++ validator/validator/engine.py
@@ -0,0 +1,8 @@
+def run_validator(xml_path: str):
+    return [
+        {
+            "code": "H00",
+            "severity": "INFO",
+            "message": "Validator SAF-T conectat cu succes"
+        }
+    ]
