3000a7b037b5171ec12be59ef0ca923d55f69f89
Ptaprogramator87 <pascu.alexandru87@gmail.com>
2026-01-28 14:34:39 +0200

    Delete validator/validator directory

--- validator/validator/engine.py
+++
@@ -1,8 +0,0 @@
-def run_validator(xml_path: str):
-    return [
-        {
-            "code": "H00",
-            "severity": "INFO",
-            "message": "Validator SAF-T conectat cu succes"
-        }
-    ]
