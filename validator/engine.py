1a86357059e8fb8283726059adec6fe745cca3c6
Ptaprogramator87 <pascu.alexandru87@gmail.com>
2026-01-30 13:12:01 +0200

    Update engine.py

--- validator/engine.py
+++ validator/engine.py
@@ -1,8 +1,13 @@
-def run_validator(xml_path: str):
-    return [
-        {
-            "code": "H00",
-            "severity": "INFO",
-            "message": "Validator SAF-T funcțional"
-        }
-    ]
+from validator.loader import load_xml
+from validator.rules import ALL_RULES
+
+def run_validator(xml_path):
+    tree = load_xml(xml_path)
+    results = []
+
+    for rule in ALL_RULES:
+        res = rule(tree)
+        if res:
+            results.append(res)
+
+    return results
